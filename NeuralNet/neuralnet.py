8be4090d4ac4010ab0da19d562f97f2159ec8294
benilak <31973046+benilak@users.noreply.github.com>
2018-02-27 13:00:43 -0700

    removing data set and tidying

--- NeuralNet/neuralnet.py
+++ NeuralNet/neuralnet.py
@@ -206,17 +206,3 @@ class NetClassifier:
                         pass
             print(cycle)
         return self.layers
-
-
-iris = pd.read_csv('iris.txt')
-trash, iris_data, iris_targets = np.hsplit(iris, [1, 5])
-iris_data = pd.DataFrame(normalize(iris_data))
-iris_targets = iris_targets.iloc[:, 0]
-data_train, data_test, targets_train, targets_test = \
-    train_test_split(iris_data, iris_targets, train_size=2/3, test_size=1/3, random_state=11)
-
-classifier = NetClassifier(data_train, targets_train, [4])
-neuralnet = classifier.build_net(rate=0.08, cycles=1000)
-model = NetModel(neuralnet)
-predictions = model.predict(data_test, targets_test)
-print(predictions[1])

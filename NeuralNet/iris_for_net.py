519246f5f5a7c2e12af83b34d09e983c7951816e
benilak <31973046+benilak@users.noreply.github.com>
2018-02-27 13:13:34 -0700

    Create iris_for_net.py

---
+++ NeuralNet/iris_for_net.py
@@ -0,0 +1,13 @@
+iris = pd.read_csv('iris.txt')
+trash, iris_data, iris_targets = np.hsplit(iris, [1, 5])
+iris_data = pd.DataFrame(normalize(iris_data))
+iris_targets = iris_targets.iloc[:, 0]
+data_train, data_test, targets_train, targets_test = \
+    train_test_split(iris_data, iris_targets, train_size=2/3, test_size=1/3, random_state=11)
+
+# as of yet, this single hidden layer with four nodes seems to produce the most accurate predictions
+classifier = NetClassifier(data_train, targets_train, [4]) 
+neuralnet = classifier.build_net(rate=0.08, cycles=1000) # arbitrary parameters, typically 0.1 < rate < 0.4
+model = NetModel(neuralnet)
+predictions = model.predict(data_test, targets_test)
+print(predictions[1])
